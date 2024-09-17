703c52eeb2638a8197a2a109078d742aba597967
Elsht908 <ekaterinashte9@gmail.com>
2024-09-18 01:03:47 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    float h, v, a, t, g, x, y, l;
+
+    cin >> h;
+    cin >> v;
+    cin >> a;
+
+    g = 9.8;
+    t = v * sin(a) * (1 + sqrt(1 + (2 * g * h / (v * pow(v, 2) * pow(sin(a), 2))))) / g;
+    x = v * t * cos(a);
+    y = h + v * t * sin(a) - 0.5 * g * pow(t, 2);
+
+    if (x < 0) {
+        x = 0;
+    }
+    if (y < 0) {
+        y = 0;
+    }
+    cout << "x=" << x << endl;
+
+    cout << "y=" << y << endl;
+    cin >> l;
+}
